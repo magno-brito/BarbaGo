b093dc7e419af761d4f868e82a6f12ab51a8a275
Yves de Sá <yvesdsb98@gmail.com>
2024-04-01 15:05:58 -0300

    Criando as pastas para o projeto

---
+++ backend/src/main/java/LES/BarbaGo/BarbaGoApplication.java
@@ -0,0 +1,13 @@
+package LES.BarbaGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbaGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbaGoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/LES/BarbaGo/BarbaGoApplicationTests.java
@@ -0,0 +1,13 @@
+package LES.BarbaGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbaGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
